8791073cc042c55601d3657320f165e72163f24d
droctopus1145 <zhouyangfan513@gmail.com>
2025-03-20 12:06:25 +0800

    3.20 finish the peoject2 code

--- project2-ai.c
+++ project2-ai.c
@@ -4,106 +4,146 @@
 typedef int ElementType;
 typedef struct TreeNode *Tree;
 typedef struct node *listptr;
+
 struct TreeNode {
     ElementType Element;
     Tree Left;
     Tree Right;
-}treenode;
-struct node{
+};  // 二叉树节点
+
+struct node {
     ElementType Element;
     int index;
-    Tree ptr;
-};
+    struct node *ptr;
+};  // 数据存储链表节点
 
 void preorder(Tree);
-Tree createtree(listptr elementtype,int num);
+Tree createtree(listptr elementtype, int num);
+int rec_bst(Tree, int);
 
-
-int main()
-{
-    Tree T1,T2;
-    int num1,i,element,pindex;
-    scanf("%d",&num1);
-    listptr elementlist1=(listptr)malloc(num1*sizeof(struct node));
-    for(i=0;i<num1;i++)
-    {
-        scanf("%d %d",&element,&pindex);
-        elementlist1[i].Element=element;
-        elementlist1[i].index=pindex;
+int main() {
+    Tree T1, T2;
+    int num1, i, element, pindex;
+    
+    // 读取第一棵树
+    scanf("%d", &num1);
+    listptr elementlist1 = (listptr)malloc(num1 * sizeof(struct node));
+    for (i = 0; i < num1; i++) {
+        scanf("%d %d", &element, &pindex);
+        elementlist1[i].Element = element;
+        elementlist1[i].index = pindex;
     }
-    T1=createtree(elementlist1,num1);
+    T1 = createtree(elementlist1, num1);
+    
+    // 读取第二棵树
     int num2;
-    scanf("%d",&num2);
-    listptr elementlist2=(listptr)malloc(num2*sizeof(struct node));
-    for(i=0;i<num2;i++)
-    {
-        scanf("%d %d",&element,&pindex);
-        elementlist2[i].Element=element;
-        elementlist2[i].index=pindex;
+    scanf("%d", &num2);
+    listptr elementlist2 = (listptr)malloc(num2 * sizeof(struct node));
+    for (i = 0; i < num2; i++) {
+        scanf("%d %d", &element, &pindex);
+        elementlist2[i].Element = element;
+        elementlist2[i].index = pindex;
     }
-    T2=createtree(elementlist2,num2);
-    //完成树的重构
-    int target;//目标数字
-    scanf("%d",&target);
-    //正常应该中序遍历后将排序后的数据录入数组，这里撒个懒，直接遍历，时间复杂度可能略高
-    /*for(i=0;i<num1;i++)
-    {
+    T2 = createtree(elementlist2, num2);
+    
+    int target; // 目标数字
+    scanf("%d", &target);
 
-    }*/
+    // 结果链表初始化
+    listptr result = (listptr)malloc(sizeof(struct node));
+    result->Element = result->index = -1;
+    result->ptr = NULL;
+
+    listptr tmp = result;
+    
+    // 遍历第一棵树，在第二棵树中查找 target - 第一棵树的元素
+    for (i = 0; i < num1; i++) {
+        int rtarget = target - elementlist1[i].Element;
+        if (rec_bst(T2, rtarget)) {
+            tmp->Element = elementlist1[i].Element;
+            tmp->index = rtarget;
+            tmp->ptr = (listptr)malloc(sizeof(struct node));
+            tmp = tmp->ptr;
+            tmp->Element = tmp->index = -1;
+            tmp->ptr = NULL;
+        }
+    }
+
+    // 如果没有找到匹配元素
+    if (result->Element == -1) {
+        printf("false\n");
+    } else {
+        printf("true\n");
+        
+        // 删除重复元素
+        listptr test1 = result;
+        while (test1 != NULL && test1->Element != -1) {
+            listptr prev = test1, curr = test1->ptr;
+            while (curr != NULL && curr->Element != -1) {
+                if (test1->Element == curr->index) {
+                    prev->ptr = curr->ptr;
+                    free(curr);
+                    curr = prev->ptr;
+                } else {
+                    prev = curr;
+                    curr = curr->ptr;
+                }
+            }
+            test1 = test1->ptr;
+        }
+        
+        // 输出结果
+        while (result != NULL && result->Element != -1) {
+            printf("%d + %d = %d\n", result->Element, result->index, target);
+            result = result->ptr;
+        }
+    }
+
     preorder(T1);
+    printf("\n");
     preorder(T2);
-    //printf("%d %d",T1,T2);
 }
 
-void preorder(Tree t)
-{
-    if(t==NULL) return;
-    printf("%d",t->Element);
+void preorder(Tree t) {
+    if (t == NULL) return;
+    printf("%d ", t->Element);
     preorder(t->Left);
     preorder(t->Right);
-}//先序遍历递归形式
+}
 
+// 创建二叉树
 Tree createtree(listptr elementtype, int num) {
-    if (num == 0) return NULL; // 如果没有元素，直接返回 NULL
+    int i;
+    Tree root = NULL;
 
-    Tree root = NULL; // 初始化根节点为空
-
-    for (int i = 0; i < num; i++) {
-        // 创建新节点
+    for (i = 0; i < num; i++) {
         Tree newNode = (Tree)malloc(sizeof(struct TreeNode));
         newNode->Element = elementtype[i].Element;
         newNode->Left = newNode->Right = NULL;
 
         if (elementtype[i].index == -1) {
-            // 处理根节点
             root = newNode;
             elementtype[i].ptr = root;
         } else {
-            // 查找父节点
-            Tree parent = NULL;
-            for (int j = 0; j < i; j++) {
-                if (elementtype[j].index == elementtype[i].index) {
-                    parent = elementtype[j].ptr;
-                    break;
-                }
+            int j;
+            for (j = 0; j < i; j++) {
+                if (elementtype[j].index == elementtype[i].index) break;
             }
-
-            if (parent == NULL) {
-                printf("Error: Parent node not found for element %d\n", elementtype[i].Element);
-                free(newNode);
-                continue;
-            }
-
-            // 插入到父节点的左/右子树
-            if (newNode->Element < parent->Element) {
+            Tree parent = elementtype[j].ptr;
+            if (elementtype[i].Element <= parent->Element) {
                 parent->Left = newNode;
             } else {
                 parent->Right = newNode;
             }
-
             elementtype[i].ptr = newNode;
         }
     }
-
     return root;
+}
+
+// 优化的 BST 查找函数
+int rec_bst(Tree t, int target) {
+    if (t == NULL) return 0;
+    if (t->Element == target) return 1;
+    return (target < t->Element) ? rec_bst(t->Left, target) : rec_bst(t->Right, target);
 }
--- project2.c
+++ project2.c
@@ -8,15 +8,16 @@ struct TreeNode {
     ElementType Element;
     Tree Left;
     Tree Right;
-}treenode;
+}treenode;//树节点
 struct node{
     ElementType Element;
     int index;
     Tree ptr;
-};
+};//数据存储数组elementtype
 
 void preorder(Tree);
 Tree createtree(listptr elementtype,int num);
+int rec_bst(Tree,int);
 
 
 int main()
@@ -45,11 +46,48 @@ int main()
     //完成树的重构
     int target;//目标数字
     scanf("%d",&target);
-    //正常应该中序遍历后将排序后的数据录入数组，这里撒个懒，直接遍历，时间复杂度可能略高
-    /*for(i=0;i<num1;i++)
+    //遍历第一棵树，在第二颗树中寻找target-第一棵树的元素(二叉搜索树),用一个链表储存找到的第一和第二棵树的元素
+    //在链表中读取第二棵树的元素，在第一棵树中寻找target-第二棵树的元素(二叉搜索树)，如果找到，则在链表中删除这个链节，最后遍历打印
+    listptr result=(listptr)malloc(sizeof(struct node));
+    result->Element=result->index=-1;//全部置-1，以确认头节点是否被空置
+    listptr tmp=result;
+    for(i=0;i<num1;i++)
     {
-
-    }*/
+        int rtarget=target-elementlist1[i].Element;
+        if(rec_bst(T2,rtarget))
+        {
+            tmp->Element=elementlist1[i].Element;
+            tmp->index=rtarget;
+            tmp->ptr=(listptr)malloc(sizeof(struct node));
+            tmp=tmp->ptr;
+            tmp->Element=tmp->index=-1;//全部置-1，以确认节点是否被空置
+        }
+    }
+    if(result->Element==-1 && result->index==-1)
+        printf("false\n");//没有元素
+    else//有元素，检查是否重复
+    {
+        printf("true\n");
+        listptr test1=result;
+        while(test1->Element!=-1 && test1->index!=-1)
+        {
+            listptr test2=result->ptr;
+            listptr store=result;
+            while(test2->Element!=-1 && test2->index!=-1)
+            {
+                if(test1->Element==test2->index)
+                    store->ptr=test2->ptr;//删除test2节点
+                test2=test2->ptr;
+                store=store->ptr;
+            }
+            test1=test1->ptr;
+        }
+    }
+    while(result->Element!=-1 && result->index!=-1)
+    {
+        printf("%d + %d = %d\n",result->Element,result->index,target);
+        result=result->ptr;
+    }    
     preorder(T1);
     printf("\n");
     preorder(T2);
@@ -64,7 +102,7 @@ void preorder(Tree t)
     preorder(t->Right);
 }//先序遍历递归形式
 
-//思路：当父节点索引为-1时为根节点，特殊处理，创建根节点并请求内存，将数据写入树的根节点，左右置空，将数据存储数组中的index+1代表现在自己的索引，并加入指向自己的指针
+//思路：使用迭代思想，当父节点索引为-1时为根节点，特殊处理，创建根节点并请求内存，将数据写入树的根节点，左右置空，将数据存储数组中的index+1代表现在自己的索引，并加入指向自己的指针
 //当父节点不为-1时，创建节点并请求内存，利用索引在数据存储数组中找到父节点，将元素大小与父节点元素大小进行比对，如果此节点元素小于父节点元素，则存入父节点左子节点，更新索引，并加入指向自己的指针，反之存入右子节点，更新索引，并加入指向自己的指针
 Tree createtree(listptr elementtype,int num)
 {
@@ -108,4 +146,15 @@ Tree createtree(listptr elementtype,int num)
     }
     
     return root;
+}
+
+int rec_bst(Tree t,int target)
+{
+    if(t==NULL) return 0;
+    else if(t->Element==target)
+        return 1;
+    else if(rec_bst(t->Left,target)==1 || rec_bst(t->Right,target)==1)
+        return 1;
+    else
+        return 0;
 }
